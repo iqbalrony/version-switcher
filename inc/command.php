89a3ff79b4360475c280a4bd9022c51d8582848d
Harun R Rayhan <harun.b13@gmail.com>
2020-09-26 14:27:59 +0600

    added command

---
+++ inc/command.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace IqbalRony;
+
+/**
+ * WP Version Switcher Commands
+ */
+class Command extends \WP_CLI_Command {
+
+	/**
+	 * Change a plugin version
+	 *
+	 * ## OPTIONS
+	 *
+	 * <slug>
+	 * : Slug of the plugin
+	 *
+	 *
+	 * <version>
+	 * : Version of the plugin you want to change to
+	 *
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp vs change akismet 4.0.0
+	 *
+	 * @when after_wp_load
+	 */
+	public function change( $args, $assoc_args ) {
+		list( $slug, $version ) = $args;
+		\WP_CLI::line( "Change version of the $slug plugin..." );
+		if ( is_wp_error( Version::version_switch( $slug, $version ) ) ) {
+			\WP_CLI::error( 'Something went wrong' );
+		}
+		\WP_CLI::success( 'Changed version successfully' );
+	}
+}
--- inc/plugin.php
+++ inc/plugin.php
@@ -1,25 +1,29 @@
 <?php
+
 namespace IqbalRony;
-if (!defined('ABSPATH')) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
 
 
 class VersionSwitcher {
 
 	private static $instance = null;
-	
+
 	public function init() {
 
 		/**
 		 * Enqueue Admin Scripts
 		 */
-		add_action('admin_enqueue_scripts', array( $this, 'admin_scripts') );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
 
 		self::include_file();
 
 		add_action( 'admin_menu', array( $this, 'menu_page' ) );
-		
-	}
 
+		$this->registerCommands();
+
+	}
 
 
 	/**
@@ -27,33 +31,31 @@ class VersionSwitcher {
 	 */
 	public function admin_scripts() {
 
-		wp_enqueue_style('wpvs-admin-css', wpvs_plugin_url('/assets/css/version-switcher-admin.css'), '', '');
+		wp_enqueue_style( 'wpvs-admin-css', wpvs_plugin_url( '/assets/css/version-switcher-admin.css' ), '', '' );
 
-		wp_enqueue_script('wpvs-admin-js', wpvs_plugin_url('/assets/js/version-switcher-admin.js'), array('jquery'), '', true);
-		
-		wp_localize_script('wpvs-admin-js','wpvs_admin_localize',array(
-			'ajax_url' => admin_url('admin-ajax.php'),
-			'wpvs_nonce' => wp_create_nonce('wpvs_version_switcher'),
-		));
+		wp_enqueue_script( 'wpvs-admin-js', wpvs_plugin_url( '/assets/js/version-switcher-admin.js' ), array( 'jquery' ), '', true );
+
+		wp_localize_script( 'wpvs-admin-js', 'wpvs_admin_localize', array(
+			'ajax_url'   => admin_url( 'admin-ajax.php' ),
+			'wpvs_nonce' => wp_create_nonce( 'wpvs_version_switcher' ),
+		) );
 	}
-
 
 
 	/**
 	 * Include files
 	 */
 	public static function include_file() {
-		require_once(wpvs_get_plugin_path('inc/version.php'));
-		require_once(wpvs_get_plugin_path('inc/switcher.php'));
-		require_once(wpvs_get_plugin_path('inc/ajax-handler.php'));
+		require_once( wpvs_get_plugin_path( 'inc/version.php' ) );
+		require_once( wpvs_get_plugin_path( 'inc/switcher.php' ) );
+		require_once( wpvs_get_plugin_path( 'inc/ajax-handler.php' ) );
 	}
-
 
 
 	/**
 	 * Add menu page
 	 */
-	public function menu_page () {
+	public function menu_page() {
 		add_menu_page(
 			__( 'Version Switcher', '@version-switcher' ),
 			__( 'Version Switcher', '@version-switcher' ),
@@ -65,20 +67,26 @@ class VersionSwitcher {
 	}
 
 
-
 	/**
 	 * Include setting page markup
 	 */
-	public function menu_page_markup () {
+	public function menu_page_markup() {
 		require_once wpvs_get_plugin_path( 'templates/settings.php' );
 	}
 
+	public function registerCommands() {
+		if ( defined( 'WP_CLI' ) && WP_CLI ) {
+			require_once wpvs_get_plugin_path( 'inc/command.php' );
+			\WP_CLI::add_command( 'vs', Command::class );
+		}
+	}
 
 
 	public static function instance() {
 		self::$instance = new self();
-        return self::$instance;
-    }
-	
+
+		return self::$instance;
+	}
+
 
 }
